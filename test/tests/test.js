e330110f815003b8cb93798b10e5f58bc4158265
Tomas Korenko <radixxko@gmail.com>
2018-09-12 11:13:49 +0200

    Fixed tests

--- test/tests/test.js
+++ test/tests/test.js
@@ -9,15 +9,15 @@ it( 'Should localize without transformations', () =>
 
 	assert.equal( localization.get( 'en', 'foo.bar'), 'foo.bar.en' );
 	assert.equal( localization.get( 'de', 'foo.bar'), 'foo.bar.de' );
-	assert.equal( localization.get( 'es', 'foo.bar'), undefined );
-	assert.equal( localization.get( 'en', 'foo.bar.goo'), undefined );
+	assert.equal( localization.get( 'es', 'foo.bar'), 'foo.bar' );
+	assert.equal( localization.get( 'en', 'foo.bar.goo'), 'foo.bar.goo' );
 	assert.equal( localization.get( 'en', 'foo.bars'), 'foo.bar.en2' );
 	assert.equal( localization.get( 'en', 'foo.bars', { user: { name: 'John' }}), 'foo.bar.en1 John' );
 	assert.equal( localization.get( 'en', 'foo.user', { username: 'John' }), 'foo.user.en user.name' );
 	assert.equal( localization.get( 'en', 'bar.foo'), 'bar.foo.en' );
-	assert.equal( localization.get( 'en', 'foobar'), undefined );
+	assert.equal( localization.get( 'en', 'foobar'), 'foobar' );
 	assert.equal( localization.get( 'en', 'foobar', 'bar.foo'), 'bar.foo.en' );
-	assert.equal( localization.get( 'en', 'foobar', 'barfoo'), undefined );
+	assert.equal( localization.get( 'en', 'foobar', 'barfoo'), 'foobar' );
 });
 
 it( 'Should localize with transformations', () =>
@@ -27,12 +27,12 @@ it( 'Should localize with transformations', () =>
 	assert.equal( localization.get( 'en', 'foo.bar'), 'FOO.BAR.EN' );
 	assert.equal( localization.get( 'de', 'foo.bar'), 'FOO.BAR.DE' );
 	assert.equal( localization.get( 'es', 'foo.bar'), 'FOO.BAR.EN' );
-	assert.equal( localization.get( 'en', 'foo.bar.goo'), undefined );
+	assert.equal( localization.get( 'en', 'foo.bar.goo'), 'foo.bar.goo' );
 	assert.equal( localization.get( 'en', 'foo.bars'), 'FOO.BAR.EN2' );
 	assert.equal( localization.get( 'en', 'foo.bars', { user: { name: 'John' }}), 'FOO.BAR.EN1 "JOHN"' );
 	assert.equal( localization.get( 'en', 'foo.user', { username: 'John' }), 'FOO.USER.EN USER.NAME' );
 	assert.equal( localization.get( 'en', 'foo.user', { username: 'John' }, { user: { name: 'Doe' }}), 'FOO.USER.EN "DOE"' );
-	assert.equal( localization.get( 'en', 'foobar'), undefined );
+	assert.equal( localization.get( 'en', 'foobar'), 'foobar' );
 	assert.equal( localization.get( 'en', 'foobar', 'foo.bar'), 'FOO.BAR.EN' );
-	assert.equal( localization.get( 'en', 'foobar', 'barfoo'), undefined );
+	assert.equal( localization.get( 'en', 'foobar', 'barfoo'), 'foobar' );
 });
